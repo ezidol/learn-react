ac3994979986368a2a779182750f1e883674c1dc
Suh <shaning7@gmail.com>
2020-08-30 01:04:29 +0900

    next lesson: #4 2 Styiling the Movies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';  
+import React from "react";
+import axios from "axios";
+import Movie from "./Movies";
 
 class App extends React.Component {
   state = {
-    isLoading: true
-  }; 
-  componentDidMount() {
-    setTimeout(()=>{
-      this.setState({ isLoading: false });
-    }, 6000);
+    isLoading: true,
+    movies: [],
   };
-
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-    <div>
-      {isLoading ? "Loading" : "We are ready"}
-    </div>
-    
-  )}
-};
-
-
+      <div>
+        {isLoading
+          ? "Loading"
+          : movies.map((movie) =>  (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              )
+            )};
+      </div>
+    );
+  }
+}
 
 export default App;
